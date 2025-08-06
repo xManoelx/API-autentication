19725978ac6278f7ce04bc25a0eaf2fc59a2a7bf
xManoelx <manoeldr03@gmail.com>
2025-08-06 20:49:58 -0300

    criando banco de dados

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from models.user import User
+from database import db
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'your_secret_key'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SECRET_KEY'] = 'your_secret_key' # Chave secreta para sessões
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db' # Configuração do banco de dados SQLite
 
+db.init_app(app) # Inicializa o objeto do banco de dados com a aplicação Flask
 
-db = SQLAlchemy(app)
+# Session <- conecxão ativa com o banco de dados
 
+# Cria a rotina Hello World
 @app.route('/hello-world', methods=['GET'])
 def hello_world():
     return 'Hello, World!'
---
+++ database.py
@@ -0,0 +1,3 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy() # Configura o objeto do banco de dados
