f6ab867facc4d2ba79769b1524d5fb32715d1d15
xManoelx <manoeldr03@gmail.com>
2025-08-06 21:30:27 -0300

    autenticando login

--- app.py
+++ app.py
@@ -1,14 +1,37 @@
-from flask import Flask
-from models.user import User
-from database import db
+from flask import Flask, request, jsonify   # Importa Flask
+from models.user import User                # Importa o modelo User
+from database import db                     # Importa o objeto do banco de dados
+from flask_login import LoginManager        # Importa o gerenciador de login
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'your_secret_key' # Chave secreta para sessões
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db' # Configuração do banco de dados SQLite
+app.config['SECRET_KEY'] = 'your_secret_key' 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db' 
 
-db.init_app(app) # Inicializa o objeto do banco de dados com a aplicação Flask
+login_manager = LoginManager() 
 
-# Session <- conecxão ativa com o banco de dados
+db.init_app(app)                           # Inicializa o objeto do banco de dados
+login_manager.init_app(app)                # Inicializa o gerenciador de login  
+
+# view login
+
+
+# Session <- conexão ativa com o banco de dados
+
+# Carrega o usuário pelo ID mantendo a segurança devido ao uso da senha criptografada
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    username = data.get('username')
+    password = data.get('password')
+
+    if username and password:
+        # Login
+        user = User.query.filter_by(username=username).first()
+
+        if user and user.password == password:  # Verifica se o usuário existe
+            return jsonify({'message': 'Login bem-sucedido'}), 200
+
+    return jsonify({'message': 'Credenciais invalidas'}), 400
 
 # Cria a rotina Hello World
 @app.route('/hello-world', methods=['GET'])
